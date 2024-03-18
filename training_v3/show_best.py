a0da1d878eff1bd1bc9ea03d6a203306c496348d
Jie-F <78284005+Jie-F@users.noreply.github.com>
2024-03-17 22:05:14 -0600

    Update show_best.py

--- training_v3/show_best.py
+++ training_v3/show_best.py
@@ -24,11 +24,17 @@ all_data = read_and_process_json_files()
 # Initialize an empty list to keep track of top 3 scores
 top_scores = []
 
+total_asteroids_hit = 0
+total_timesteps_simulated = 0
+
 for run in all_data:
     current_score = run['team_1_total_asteroids_hit']
     current_chromosome = run['chromosome']
     # Append the current run's score and chromosome to the list
     top_scores.append((current_score, current_chromosome))
+
+    total_asteroids_hit += run["team_1_total_asteroids_hit"]
+    total_timesteps_simulated += run['neo_total_sim_ts']
 
 # Sort the list by score in descending order and keep only the top 3
 top_scores = sorted(top_scores, key=lambda x: x[0], reverse=True)[:15]
@@ -36,3 +42,6 @@ top_scores = sorted(top_scores, key=lambda x: x[0], reverse=True)[:15]
 # Print the top 3 scores and their chromosomes
 for i, (score, chromosome) in enumerate(top_scores, start=1):
     print(f"Top {i} score is {score} with chromosome {chromosome}")
+
+print(f"Total asteroids hit: {total_asteroids_hit}")
+print(f"Total years simulated: {total_timesteps_simulated/30/3600/24/365:.03f}")
